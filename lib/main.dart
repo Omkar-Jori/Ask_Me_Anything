c52471e4cbb8a038bed68298b64f484907fbfe2a
Omkar Jori <54843957+Omkar-Jori@users.noreply.github.com>
2020-02-20 20:24:47 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
+void main()
+{
+  runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue,
+          ),
+          body: Ball(),
+        ),
       ),
     );
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballChange=1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  ballChange=Random().nextInt(5)+1;
+                });
+              },
+              child: Image.asset('images/ball$ballChange.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
